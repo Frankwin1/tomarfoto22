f421ae1f8e13161610116e3d89d6d0f5424d87e9
Frank <frankpt18@gmail.com>
2020-02-26 08:59:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/randomizer/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.randomizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView mimageView;
+    private static final int REQUEST_IMAGE_CAPTURE = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mimageView = findViewById(R.id.imageView);
+        }
+        public void tomarFoto(View view){
+        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (imageTakeIntent.resolveActivity(getPackageManager())!=null)
+            {
+                startActivityForResult(imageTakeIntent,REQUEST_IMAGE_CAPTURE);
+            }
+
+        }
+        @Override
+    protected void onActivityResult(int requestCode,int resultCode, Intent data)
+        {
+         if(requestCode==REQUEST_IMAGE_CAPTURE && resultCode==RESULT_OK)
+         {
+             Bundle extras = data.getExtras();
+             Bitmap imageBitmap=(Bitmap) extras.get("data");
+             mimageView.setImageBitmap(imageBitmap);
+
+         }
+
+        }
+
+}
